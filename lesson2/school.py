082623dde299cb916afb09f036ff1d2096c276cd
Dmitriy Stanevich <stan_1@list.ru>
2018-12-13 01:58:48 +0300

    пилим цикл for

--- lesson2/school.py
+++ lesson2/school.py
@@ -1,17 +1,15 @@
-users = [
-{'school_class': '4a', 'scores': [5,5,5,5,4]},
-{'school_class': '4a', 'scores': [5,4,5,5,3]},
-{'school_class': '5a', 'scores': [5,5,4,4,3]},
-{'school_class': '4b', 'scores': [4,4,4,4,4]},
-{'school_class': '6a', 'scores': [4,3,4,3,5]},
-{'school_class': '7a', 'scores': [5,5,5,5,4]} ]
+list_of_classes = [
+	{'school_class': '4a', 'scores': [5,5,5,5,4]},
+	{'school_class': '1a', 'scores': [5,4,5,5,3]},
+	{'school_class': '5a', 'scores': [5,5,4,4,3]},
+	{'school_class': '4b', 'scores': [4,4,4,4,4]},
+	{'school_class': '6a', 'scores': [4,3,4,3,5]},
+	{'school_class': '7a', 'scores': [5,5,5,5,4]} 
+]
 
-for x in users:
-	score_user=x['scores']
-	av_score_user=sum(score_user)/len(score_user)
-	print(av_score_user)
+for school_class in list_of_classes:
+	av_score_class = sum(school_class['scores']) / len(school_class['scores'])
+	print(av_score_class)
 
-print(len(users))
-
-
-	
+school_av_score = sum(sum(school_class['scores'])/(len(school_class['scores'])) for school_class in list_of_classes) / len(list_of_classes)
+print('Средняя оценка в школе: ',  school_av_score)
