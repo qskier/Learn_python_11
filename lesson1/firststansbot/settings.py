0a286c8852d822388877eb758fdd3d27beac81d0
Dmitriy Stanevich <stan_1@list.ru>
2018-12-16 20:25:04 +0300

    разбираюсь с gitignore

--- lesson1/firststansbot/settings.py
+++
@@ -1,4 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-	'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-API_KEY = "779932182:AAG63GxuW27qxS9X4xblDCr1hzEeWeYQ_tg"
